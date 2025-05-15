17a8c23ff49d144b6f889549531775a4942103d0
Ghayaythri-Saikarthik <Ghayathriprasanna@gmail.com>
2025-05-15 07:54:11 +0530

    New Rule

--- Github1.js
+++ Github1.js
@@ -1,6 +1,3 @@
 console.log("Task Assigned Completed for Day 1");
-<<<<<<< HEAD
 console.log("Reassignment Opted for the Task1");
-=======
 console.log("Task Reassigned Completed");
->>>>>>> d28ef89ebab915c479f05d052cbdc7fa7b24daa0
